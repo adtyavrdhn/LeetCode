c776bc024c43e7ac239d5fe562ae932c0250a4f2
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-01-29 09:19:09 +0530

    Time: 132 ms (91.36%), Space: 84.5 MB (79.42%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,24 +11,39 @@
  */
 class Solution {
 public:
-    vector<int> t;
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) 
     {
-        inorder(root1);
-        inorder(root2);
-        sort(t.begin(),t.end());
-        
-        return t;
-    }
+      vector<int> res;
+    stack<TreeNode*> s1,s2;
     
-    void inorder(TreeNode* r)
+    while(root1||root2|| !s1.empty()||!s2.empty())//when both roots and stacks are not empty
     {
-     if(r)
-     {
-        inorder(r->left);
-        t.push_back(r->val);
-        inorder(r->right);
-         
-     }
+        while(root1!=NULL)//when root-1 is not null, then push root value in stack s1 and go to root's left
+        {
+            s1.push(root1);
+            root1=root1->left;
+        }
+        while(root2!=NULL)//when root-2 is not null,then push it on stack s2 and move to root's left
+        {
+            s2.push(root2);
+            root2=root2->left;
+        }
+        if(s2.empty()||(!s1.empty() && s1.top()->val<=s2.top()->val))//if s1 top is less then s2 top means s1 top element is smaller then s2-top element ,
+		//then push that value in ans vector and check for it's right
+        {
+            root1=s1.top();
+            s1.pop();
+            res.push_back(root1->val);
+            root1=root1->right;
+        }
+        else
+        {
+            root2=s2.top();
+            s2.pop();
+            res.push_back(root2->val);
+            root2=root2->right;
+        }
+    }
+    return res;
     }
 };
