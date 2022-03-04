18a4733d9924c6e4cd76d617fdd2fb23b62f9560
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-03-04 21:20:41 +0530

    Time: 0 ms (100.00%), Space: 12 MB (93.49%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -6,22 +6,17 @@ public:
         int k, l;
         
         for(k=n-1;k>=0;k--)
-        {
             if(nums[k]<nums[k+1])break;
-        }
+        
         
         if(k<0) reverse(nums.begin(),nums.end());
         
         else
         {
             for(l=n;l>k;l--)
-            {
                 if(nums[k]<nums[l]) break;
-            }
             
-            
-            swap(nums[k],nums[l]);
-            
+            swap(nums[k],nums[l]);  
             reverse(nums.begin()+k+1,nums.end());
         }
         
