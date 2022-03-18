2aaae4d2a5e96c44ab63988036d4b8e34f8964f8
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-03-18 19:12:09 +0530

    Time: 20 ms (22.33%), Space: 8.5 MB (18.57%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    string removeKdigits(string num, int k) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
+    string removeKdigits(string num, int k) 
+    {
+        string res;
         
         int n = num.size();
-        stack<char> mystack;
-        //Store the final string in stack
-        for(char c: num)
+        for(int i=0;i<n;i++)
         {
-            while(!mystack.empty() && k>0 && mystack.top()>c)
-            {   mystack.pop();      k-=1;   }
             
-            if(!mystack.empty() || c!='0')
-                mystack.push(c);
+            while(res.size() && res.back() > num[i] && k > 0)
+            {
+                k--;
+                res.pop_back();
+            }
+            
+            if(num[i]!= '0' || res.size())
+                res.push_back(num[i]);
         }
         
-        //Now remove the largest values from the top of the stack
-        while(!mystack.empty() && k--)
-            mystack.pop();
-        if(mystack.empty())
+        while(res.size() && k > 0)
+        {
+            k--;
+            res.pop_back();
+        }
+        
+        if(res=="")
             return "0";
         
-        //Now retrieve the number from stack into a string (reusing num)
-        while(!mystack.empty())
-        {
-            num[n-1] = mystack.top();
-            mystack.pop();
-            n-=1;
-        }
-        return num.substr(n);
+        return res;
     }
 };
