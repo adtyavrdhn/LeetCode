1f74400c20540235ee2a26b51921f0658f1d7871
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-03-19 15:31:58 +0530

    Time: 5 ms (17.80%), Space: 6 MB (95.15%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string breakPalindrome(string s)
+    {
+        int n = s.size();
+        if(n==1)
+            return "";
+        int count = 0;
+      
+        for(int i=0;i<n/2;i++)
+        {
+            if(s[i]!='a')
+            {
+                s[i]='a';
+                return s;
+            }
+        }
+        
+            s[n-1]='b';
+        
+        
+        return s;
+    }
+};
