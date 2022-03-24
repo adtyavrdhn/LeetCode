ba6235de7197d49bd58715f4523ae336e6552475
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-03-24 19:00:04 +0530

    Time: 448 ms (17.15%), Space: 84.5 MB (48.45%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,48 +1,26 @@
 class Solution {
 public:
-    int x;
     int minimizedMaximum(int n, vector<int>& q) 
     {
         int left  = 1;
-        int right = (int)1e9;
-        int res = 0;
+        int right = (int)1e6;
         
-        // if x is the number which shows the maximum number of products that can be given to a store at any time we need to minimize it
-        // sum of quantities <= n(total stores) * x should be the condition for the distribution to be feasible
-        
-        // int sum = 0;
-        // for(int i=0;i<size(q);i++)
-        //     sum+=q[i];
-        
-        while(left <= right)
+        while(left < right)
         {
-            int mid = left+(right-left)/2;
-            
-            if(feasible(mid,n,q))
+            int mid = (left + right)/2;
+            int sum = 0;
+            for(int i=0;i<q.size();i++)
             {
-                // let's search in the left half to see if we can go lower than the current mid
-                res=mid;
-                right=mid-1;
+                sum+=((q[i]+mid-1)/mid);
+                    // +mid -1 is the same as ceil() fncn
             }
             
-            else
+            if(sum>n)
                 left = mid + 1;
+            else
+                right = mid;
         }
         
-        return res;
-    }
-    
-    bool feasible(int x, int n, vector<int>& q)
-    {
-        for(int i=0;i<size(q);i++)
-        {
-            n-= ceil(q[i]/(x*1.0));
-            
-            if(n<0)
-                return false;
-        }
-        return true;
-        
-        
+        return left;
     }
 };
