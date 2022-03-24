f1ea322c90c0fb6165fbc0d3b9faeceee17422a1
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-03-24 17:03:44 +0530

    Time: 259 ms (35.49%), Space: 46.4 MB (22.72%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -2,33 +2,49 @@ class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) 
     {
-        int r = mat.size();
-        int c = mat[0].size();
+        int n = size(mat);
+        int top = 0;
+        int bottom = n-1;
         
-        int i=0,j=0;
-        
-        while(i>=0 && i<r && j>=0 && j<c)
+        while(top<=bottom)
         {
-            int x = (i-1 <0)?-1:mat[i-1][j];
-            int y = (j-1 <0)?-1:mat[i][j-1];
-            int z = (j+1==c)?-1:mat[i][j+1];
-            int w = (i+1==r)?-1:mat[i+1][j];
+            int midrow = top + (bottom-top)/2;
             
-            if(mat[i][j]>x && mat[i][j]>y && mat[i][j]>z && mat[i][j]>w)
-                return {i,j};
+            int rowmax = maxinarow(mat[midrow]);
+            if(midrow==0)
+            {
+                if(mat[midrow][rowmax] > mat[midrow+1][rowmax])
+                    return {midrow,rowmax};
+            }
             
-             int p = max({x,y,z,w});
+            if(midrow==n-1)
+            {
+                if(mat[midrow][rowmax] > mat[midrow-1][rowmax])
+                    return {midrow,rowmax};
+            }
             
-            if(p==x)
-                i--;
-            else if(p==y)
-                j--;
-            else if(p==w)
-                i++;
+            if((mat[midrow][rowmax] > mat[midrow+1][rowmax])
+               &&(mat[midrow][rowmax] > mat[midrow-1][rowmax]))
+                return {midrow,rowmax};
+            
+            if(mat[midrow][rowmax]<mat[midrow+1][rowmax])
+                top = midrow+1;
             else
-                j++;
+                bottom = midrow-1;
         }
         
-        return {};
+        return {-1,-1};
+    }
+    
+    int maxinarow(vector<int>& arr)
+    {
+        int maxi = 0;
+        for(int i=0;i<size(arr);i++)
+        {
+            if(arr[i]>arr[maxi])
+                maxi = i;
+        }
+        
+        return maxi;
     }
 };
