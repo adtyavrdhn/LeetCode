ca912a0f56dd9ff08db63d5550d7cc0e44394235
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-03-30 20:01:42 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (6.37%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,46 +1,44 @@
 class Solution {
 public:
-    int rob(vector<int>& arr) 
+    int rob(vector<int>& nums) 
     {
-    int n = arr.size();
-        if(n==1) return arr[0];
-    vector<int> arr1;
-    vector<int> arr2;
-    
-    for(int i=0; i<n; i++)
-    {
+        int n = size(nums);
+        if(n==1)
+            return nums[0];
         
-        if(i!=0) arr1.push_back(arr[i]);
-        if(i!=n-1) arr2.push_back(arr[i]);
-    }
-    
-     int ans1 = solve(arr1);
-     int ans2 = solve(arr2);
-    
-    return max(ans1,ans2);
+       vector<int> t1;
+        vector<int>t2;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(i!=0) t1.push_back(nums[i]);
+            if(i!=n-1) t2.push_back(nums[i]);
+        }
+        
+        int res1 = helper(t1);
+        int res2 = helper(t2);
+        
+        return max(res1,res2);
        
     }
     
-    int solve(vector<int>& arr)
+    int helper(vector<int>& nums)
     {
-     int n = arr.size();
-     int prev = arr[0];
-     int prev2 =0;
-    
-    for(int i=1; i<n; i++)
-    {
-         int pick = arr[i];
-        if(i>1) pick += prev2;
+        int n = size(nums);
+         vector<int> dp(n,0);
         
-        int nonPick = 0 + prev;
+        dp[0]=nums[0];
         
-        int cur = max(pick, nonPick);
-        prev2 = prev;
-        prev= cur;
+        for(int i=1;i<n;i++)
+        {
+           int take = nums[i];
+            if(i>1) take+=dp[i-2];
+            
+            int dont = dp[i-1];
+            
+            dp[i] = max(take,dont);
+        }
         
-    }
-        
-    return prev;
-        
+        return dp[n-1];
     }
 };
