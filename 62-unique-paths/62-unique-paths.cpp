2d979fa94c0e551650ea87814874e92ff9675593
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-03-31 21:14:33 +0530

    Time: 3 ms (35.30%), Space: 6.6 MB (25.41%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -4,16 +4,16 @@ public:
     {
         vector<vector<int>> dp(m,vector<int>(n,0));
         
-        for(int i=0;i<n;i++) dp[0][i]=1;
-        
-        
-        for(int i=0;i<m;i++) dp[i][0] = 1;
-        
-        for(int i=1;i<m;i++)
-            for(int j=1;j<n;j++)
-                dp[i][j] = dp[i-1][j]+dp[i][j-1];
-           
-        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 || j==0) dp[i][j]=1;
+                
+                 else dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+               
+        }
         
         return dp[m-1][n-1];
     }
