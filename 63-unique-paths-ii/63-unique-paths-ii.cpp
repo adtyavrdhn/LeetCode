ad5e2d6a5f743808cb59b00fb4bb8774c1826a8f
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-03-31 22:05:26 +0530

    Time: 3 ms (80.74%), Space: 7.5 MB (88.32%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,22 +1,58 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& g) 
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
     {
-        int m = g.size();
-        int n = g[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,g,dp);
-    }
-    int f(int i,int j, vector<vector<int>>& g,vector<vector<int>> &dp)
-    {
-        if(i==0 && j==0 && g[i][j]==1) return 0;
-        if(i==0 && j==0 && g[i][j]==0) return 1;
-        if(i<0 || j<0 ||g[i][j]==1) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        int arr[m][n];
+        for(int i=0;i<m;i++)
+        {
+            if(obstacleGrid[i][0]==1)
+                {
+                    arr[i][0]=0;
+                    for(int j=i+1;j<m;j++)
+                    {
+                        arr[j][0]=0;
+                    }
+                break;
+                }
+            else
+            {
+                    arr[i][0]=1;
+            }
+        }
+         for(int i=0;i<n;i++)
+        {
+            if(obstacleGrid[0][i]==1)
+                {
+                    arr[0][i]=0;
+                 for(int j=i+1;j<n;j++)
+                    {
+                        arr[0][j]=0;
+                    }
+                    break;
+                }
+            else
+            {
+                    arr[0][i]=1;
+            }
+        }
+         for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(obstacleGrid[i][j]==1)
+                {
+                    arr[i][j]=0;
+                }
+                
+                else
+                {
+                    arr[i][j]=arr[i-1][j]+arr[i][j-1];
+                }  
+            }
+        }
+        return arr[m-1][n-1];
         
-        int up = f(i-1,j,g,dp);
-        int left = f(i,j-1,g,dp);
-        
-        return dp[i][j]=left+up;
     }
 };
