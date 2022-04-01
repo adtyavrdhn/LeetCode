c5568196d6de46e5f2639206ded5be3ced1f8be4
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-04-01 11:20:35 +0530

    Time: 14 ms (45.69%), Space: 10 MB (47.47%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -4,51 +4,40 @@ public:
     {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n,0));
-       
+        
+        
+        vector<int> prev(n,0);
         for(int i=0;i<m;i++)
         {
+            vector<int> curr(n,0);
+            
             for(int j=0;j<n;j++)
             {
                 if(i==0 && j==0)
-                    dp[i][j]=grid[i][j];
+                    curr[j]=grid[i][j];
                 else
                 {
                      int left = grid[i][j];
                      int up = grid[i][j];
                     
                     if(j>0)
-                        left += dp[i][j-1];
+                        left += curr[j-1];
                     else
                         left+=1e9;
                     
                     if(i>0)
-                        up+=dp[i-1][j];
+                        up+=prev[j];
                     else
                         up+=1e9;
                     
                     
-                    dp[i][j]=min(up,left);
+                    curr[j]=min(up,left);
                 }
             }
+            
+            prev = curr;
         }
         
-        return dp[m-1][n-1];
-        
-        // return helper(grid,m-1,n-1,dp);
+        return prev[n-1];  
     }
-    
-//     int helper(vector<vector<int>>& grid, int i, int j,vector<vector<int>> &dp)
-//     {
-//         if(i<0 || j<0) return 1e9;
-        
-//         if(i==0 && j==0) return grid[0][0];
-        
-//         if(dp[i][j]!= -1) return dp[i][j];
-        
-//         int left = grid[i][j]+ helper(grid,i,j-1,dp);
-//         int up = grid[i][j] + helper(grid,i-1,j,dp);
-        
-//         return dp[i][j]= min(left,up);
-//     }
 };
