8d93f347489a93c9d3a5e3d9d99e8582f30f249c
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-04-05 23:18:12 +0530

    Time: 44 ms (36.97%), Space: 18 MB (41.87%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -3,57 +3,39 @@ public:
     int change(int target, vector<int>& nums) 
     {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int> (target+1,0));
+        // vector<vector<int>> dp(n,vector<int> (target+1,0));
+        vector<int> prev(target+1,0);
+        prev[0]=1;
         
-        for(int i=0;i<n;i++)
-        {
-            dp[i][0]=1;
-        }
+//         for(int i=0;i<n;i++)
+//         {
+//             dp[i][0]=1;
+//         }
         
         for(int i=0;i<=target;i++)
         {
             if(i%nums[0]==0)
-                dp[0][i]=1;
+                prev[i]=1;
         }
         
         for(int i=1;i<n;i++)
         {
-            for(int j=1;j<=target;j++)
+            vector<int> curr(target+1,0);
+            
+            for(int j=0;j<=target;j++)
             {
                 int pick = 0;
                 if(nums[i]<=j)
-                    pick = dp[i][j-nums[i]];
+                    pick = curr[j-nums[i]];
                 
-                int npick = dp[i-1][j];
+                int npick = prev[j];
                 
-                dp[i][j] = (pick+npick);
+                curr[j] = (pick+npick);
             }
+            prev = curr;
         }
         
         
-        return dp[n-1][target];
+        return prev[target];
     }
-//     int f(vector<int>&nums , int index, int target,vector<vector<int>> &dp)
-//     {
-//         if(target==0) return 1;
-        
-        
-//         if(index==0)
-//         {
-//             if(target%nums[index]==0)
-//                 return 1;
-//             else
-//                 return 0;
-//         }
-        
-//         if(dp[index][target]!=-1) return dp[index][target];
-         
-//         int pick = 0;
-//         if(nums[index]<=target)
-//          pick = f(nums,index,target-nums[index],dp);
-        
-//         int npick = f(nums,index-1,target,dp);
-        
-//         return dp[index][target] = (pick+npick);
-//     }
 };
