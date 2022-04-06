bea4bb21402655e06fc077a8cae7ae235b0b2723
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-04-06 09:32:06 +0530

    Time: 217 ms (61.57%), Space: 10.4 MB (74.83%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    int threeSumMulti(vector<int>& nums, int target) 
+    {
+        long long res = 0;
+        sort(nums.begin(),nums.end());
+        // unordered_map<int,int> mpp;
+        int n = size(nums);
+//       for(int i=0;i<n;i++)
+//       {
+//           mpp[nums[i]]++;
+//       }
+        
+        int count = 0;
+        
+        for(int i=0;i<n-2;i++)
+        {
+            int T = target-nums[i];
+            int lo = i+1;
+            int hi = n-1;
+              while(lo < hi)
+             {
+                int sum = nums[lo]+nums[hi];
+                  
+              if(sum < T)
+                  lo++;
+              else if(sum > T) 
+                   hi--;
+              else if(nums[lo]!=nums[hi])
+              {
+                      int left = 1;
+                      int right = 1;
+                   while(lo+1 < hi && nums[lo]==nums[lo+1])
+                   {
+                       left++;
+                       lo++;
+                   }
+                  
+                  while(lo + 1 < hi && nums[hi]==nums[hi-1])
+                  {
+                      right++;
+                      hi--;
+                  }
+                  
+                  res+=left*right;
+                  res%=mod;
+                  
+                  lo++;
+                  hi--;
+              }
+                else
+                 {
+                      // M = hi - lo + 1
+                    // contributed M*(M-1)/2 
+                    
+                    res+=(hi-lo + 1)*(hi - lo)/2;
+                    res%=mod;
+                    break;
+                 }
+             }
+                
+          
+        }
+        // cout<<endl;
+        return (int)res;
+    }
+};
