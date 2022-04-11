094cb275d4b42b03680de6ca08cd78294c253d6a
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-04-11 12:56:29 +0530

    Time: 32 ms (71.23%), Space: 13.8 MB (87.46%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -3,10 +3,11 @@ public:
     int m=0, n=0;
     void reverse(vector<vector<int>> &grid, int l, int r)
     {
+        //treating the 2d matrix as a 1d array
         while(l<r)
         {
-            int lr = l/n, lc = l%n;
-            int rr = r/n, rc = r%n;
+            int lr = l/n, lc = l%n; // row,col of first element
+            int rr = r/n, rc = r%n; // row,col of last element
             
             swap(grid[lr][lc],grid[rr][rc]);
             
@@ -19,6 +20,9 @@ public:
         m = grid.size(), n = grid[0].size();
         k = k%(m*n);
         if(k == 0) return grid;
+        ////
+// Could also have stored all the elements in a 1d array and then performed only the rotate array algorithm
+        // Check Rotate Array Algorithm
         reverse(grid, 0, m*n-1);
         reverse(grid, 0, k-1);
         reverse(grid, k, m*n-1);
