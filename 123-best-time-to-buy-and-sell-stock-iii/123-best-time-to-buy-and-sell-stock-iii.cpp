6bde5c5bbeecb7d080d0f1affe3023e36a423244
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-04-13 16:24:24 +0530

    Time: 757 ms (14.99%), Space: 207.2 MB (13.75%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,28 +3,31 @@ public:
     int maxProfit(vector<int>& prices) 
     {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int> (3,-1)));
-        return f(prices,0,true,2,dp);
-    }
-    
-    int f(vector<int>& prices, int index, bool buy, int count,vector<vector<vector<int>>>&dp)
-    {
-        if(count==0) return 0;
-        if(index==prices.size())
-            return 0;
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2,vector<int> (3,0)));
+     
         
-        if(dp[index][buy][count]!=-1) return dp[index][buy][count];
         
-        int profit = 0;
-        if(buy)
+        for(int i=n-1;i>=0;i--)
         {
-            profit = max((-prices[index]+f(prices,index+1,false,count,dp)),f(prices,index+1,true,count,dp));
-        }
-        else
-        {
-            profit = max((prices[index]+f(prices,index+1,true,count-1,dp)),f(prices,index+1,false,count,dp));
+            for(int j=0;j<=1;j++)
+            {
+                int profit = 0;
+                for(int k=1;k<=2;k++)
+                {                 
+                if(j)
+                 profit = max(-prices[i]+dp[i+1][0][k],dp[i+1][1][k]);
+        
+                else
+                profit = max(prices[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
+        
+        
+                 dp[i][j][k] = profit;
+                }
+            }
         }
         
-        return dp[index][buy][count] = profit;
+        
+        return dp[0][1][2];
+        
     }
 };
