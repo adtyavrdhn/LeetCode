8f366064728133c0671121d697e206753f00987d
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-04-13 16:32:33 +0530

    Time: 252 ms (39.49%), Space: 75.4 MB (69.87%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,7 +3,9 @@ public:
     int maxProfit(vector<int>& prices) 
     {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2,vector<int> (3,0)));
+      
+        vector<vector<int>> after(2,vector<int> (3,0));
+        vector<vector<int>> curr(2,vector<int> (3,0));
      
         
         
@@ -15,19 +17,21 @@ public:
                 for(int k=1;k<=2;k++)
                 {                 
                 if(j)
-                 profit = max(-prices[i]+dp[i+1][0][k],dp[i+1][1][k]);
+                 profit = max(-prices[i]+after[0][k],after[1][k]);
         
                 else
-                profit = max(prices[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
+                profit = max(prices[i]+after[1][k-1],after[0][k]);
         
         
-                 dp[i][j][k] = profit;
+                 curr[j][k] = profit;
                 }
             }
+            
+            after = curr;
         }
         
         
-        return dp[0][1][2];
+        return after[1][2];
         
     }
 };
