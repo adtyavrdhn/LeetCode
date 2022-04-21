6ca0ad2fede6ff11b406e5052121bfef2d681155
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-04-21 07:51:32 +0530

    Time: 11 ms (69.81%), Space: 10.4 MB (79.17%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,29 +1,72 @@
 class Solution
 {
     public:
-        vector<bool> unlocked;
-    void dfs(vector<vector < int>> &rooms, int i)
+        vector<int> parent;
+    vector<int> rank;
+
+    int find(int x)
     {
-        if (unlocked[i])
-            return;
-
-        unlocked[i] = true;
-
-      
-        for (auto key: rooms[i])
-            dfs(rooms, key);
+        return parent[x] == x ? x : find(parent[x]);
     }
-    public:
-        bool canVisitAllRooms(vector<vector < int>> &rooms)
+
+    void union_op(int i, int j)
+    {
+        int x = find(i);
+        int y = find(j);
+
+        if (x != y)
         {
-            unlocked = vector<bool> (rooms.size());
-
-            dfs(rooms, 0);
-
-            for (auto x: unlocked)
-                if (!x)
-                    return false;
-
-            return true;
+            if (rank[x] >= rank[y])
+            {
+                parent[y] = x;
+                rank[x]++;
+            }
+            else
+            {
+                parent[x] = y;
+                rank[y]++;
+            }
         }
+    }
+
+    bool canVisitAllRooms(vector<vector < int>> &rooms)
+    {
+        int n = rooms.size();
+        parent.resize(n);
+        rank.resize(n,1);
+        
+        for(int i=0;i<n;i++)
+            parent[i]=i;
+        
+        queue<int> q;
+        q.push(0);
+
+        while (!q.empty())
+        {
+            int i = q.front();
+            q.pop();
+
+            if (i!= 0 && parent[i]==0)
+                continue;
+
+            parent[i] = 0;
+
+            for (int key: rooms[i])
+            {
+                int x = find(key);
+                if (x!=0)
+                {
+                    q.push(key);
+                }       
+            }
+                
+        }
+
+        for(int i=0;i<parent.size();i++)
+        {
+            if(parent[i]!=0)return false;
+              // cout<<parent[i]<<" ";
+        }
+        return true;
+    }
 };
