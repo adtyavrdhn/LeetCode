bea86f5b669b56ea0340ecabd5e12d249475e9eb
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-04-25 08:50:33 +0530

    Time: 2 ms (79.45%), Space: 7.4 MB (88.55%) - LeetHub

---
+++ 284-peeking-iterator/284-peeking-iterator.cpp
@@ -0,0 +1,57 @@
+/*
+ * Below is the interface for Iterator, which is already defined for you.
+ * **DO NOT** modify the interface for Iterator.
+ *
+ *  class Iterator {
+ *		struct Data;
+ * 		Data* data;
+ *  public:
+ *		Iterator(const vector<int>& nums);
+ * 		Iterator(const Iterator& iter);
+ *
+ * 		// Returns the next element in the iteration.
+ *		int next();
+ *
+ *		// Returns true if the iteration has more elements.
+ *		bool hasNext() const;
+ *	};
+ */
+
+class PeekingIterator : public Iterator {
+public:
+    int i;
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) 
+    {
+        i = Iterator::next();
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() 
+    {
+        return i;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() 
+    {
+	    int t = i;
+        if(Iterator::hasNext())
+        {
+            i = Iterator::next();
+        }
+        else
+            i = NULL;
+        
+        return t;
+	}
+	
+	bool hasNext() const 
+    {
+	  return i!=NULL;  
+	}
+};
