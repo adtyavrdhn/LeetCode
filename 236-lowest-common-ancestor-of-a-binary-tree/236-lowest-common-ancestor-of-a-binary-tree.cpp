d1cb180cc7543834d13079490a1a8c26a1404d8a
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-05-18 20:02:23 +0530

    Time: 15 ms (88.91%), Space: 14.2 MB (60.27%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,30 +1,34 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
  */
-class Solution
-{
-    public:
-        TreeNode* dfs(TreeNode *root, TreeNode *p, TreeNode *q)
-        {
-            if (root == p || root == q || !root)
-                return root;
-
-            TreeNode *left = dfs(root->left, p, q);
-            TreeNode *right = dfs(root->right, p, q);
-            if (left && right)
-                return root;
-            else
-                return left ? left : right;
-        }
-
-    TreeNode* lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
     {
-        return dfs(root, p, q);
+        return helper(root,p,q);
+    }
+    TreeNode* helper(TreeNode* root,TreeNode* p, TreeNode* q)
+    {
+        if(p==root || q==root | !root)
+        {
+            return root;
+        }
+        
+        TreeNode* left = helper(root->left,p,q);
+        TreeNode* right = helper(root->right,p,q);
+        
+        if(left && right)
+        {
+            return root;
+        
+        }
+        
+        return left ? left : right;
     }
 };
