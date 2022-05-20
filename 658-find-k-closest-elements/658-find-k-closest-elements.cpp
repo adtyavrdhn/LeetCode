5443fe5e8a5f3728642ab5d3125b4fe3be538e32
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-05-21 02:23:21 +0530

    Time: 103 ms (31.65%), Space: 37.5 MB (10.47%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) 
-    { 
-        int n = arr.size();
-        int low = 0;
-        int high = n-1;
-        
-        int index = -1;
-        
-        while(low < high)
-        {
-            int mid = (low+high)/2;
-                 
-            if(arr[mid] < x)
-                low = mid + 1;
-            
-            else
-                high = mid;
-        }
-        
-        index = high;
-        
-        
-        int l_index = min(n-1,index+k);
-        int f_index = max(0,index-k);
-        
-        
-        while (l_index  - f_index >= k) {
-            if (x - arr[f_index] <= arr[l_index ] - x) --l_index ;
-            else ++f_index;
-        }
-        
-        
-        vector<int> res;
-        
-        for(int i=f_index;i<=l_index;i++)
-        {
-            res.push_back(arr[i]);
-        }
-        
-        return res;
-        
-    }
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+
+    // making a min-heap of absolute difference and corresponding array value
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    // store the difference one by one with the array value
+	for (int i = 0; i < arr.size(); i++)
+		pq.push({abs(arr[i] - x), arr[i]});
+
+	vector<int> ans;
+
+    // get the top of the heap i.e. the element with minimum absolute difference
+	for (int i = 0; i < k; i++)
+	{
+		ans.push_back(pq.top().second);
+		pq.pop(); // pop it k times
+	}
+
+	sort(ans.begin(), ans.end());
+	return ans;
+}
 };
