407dd853c31625f05f5ad7a03cd011c8d6c86d02
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-06-24 20:44:03 +0530

    Time: 448 ms (10.07%), Space: 59 MB (25.54%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -4,12 +4,14 @@ public:
     {
       int n = graph.size();
         
-        vector<int> indegree(n, 0), al[n];
+        vector<int> outdegree(n, 0), al[n];
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < graph[i].size(); j++){
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < graph[i].size(); j++)
+            {
                 al[graph[i][j]].push_back(i);
-                indegree[i]++;
+                outdegree[i]++;
             }
         }
         
@@ -17,33 +19,41 @@ public:
         
         vector<int> res;
         
-        for(int i = 0; i < n; i++){
-            if(indegree[i] == 0){
+        for(int i = 0; i < n; i++)
+        {
+            if(outdegree[i] == 0){
                 q.push(i);
             }
         }
         
-        while(!q.empty()){
+        while(!q.empty())
+        {
             int sz = q.size();
             bool flag = false;
-            while(sz--){
+            while(sz--)
+            {
                 
                 int curr = q.front();
+                cout<<curr<<" ";
                 q.pop();
                 
                 res.push_back(curr);
                 
-                for(int i = 0; i < al[curr].size(); i++){
+                for(int i = 0; i < al[curr].size(); i++)
+                {
                     int child = al[curr][i];
-                    indegree[child]--;
-                    if(indegree[child] == 0){
+                    outdegree[child]--;
+                    if(outdegree[child] == 0)
+                    {
                         q.push(child);
                         flag = true;
                     }
                 }
             }
+            
             if(!flag) break;
         }
+        
         sort(res.begin(),res.end());
         return res;
     }
