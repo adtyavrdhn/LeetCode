619b0736cd1dd391468003bc5e52ace7802fef01
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-06-26 12:29:25 +0530

    Time: 11 ms (100.00%), Space: 5.9 MB (75.00%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -2,38 +2,26 @@ class Solution {
 public:
     int mod = 1e9+7;
     
-    int countHousePlacements(int n) 
-    {
-        vector<vector<long long>> dp(n+1, vector<long long> (3,-1));
+     int countHousePlacements(int N) {
+        // if N = 1 total number of ways are 4 = 2*2
+        // if N = 2 total number of ways are 9 = 3*3
+        // if N = 3 total number of ways are 25 = 5*5
+        // this forms a fibonacci series -> 2, 3, 5, 8, ... => current number = sum of previous two numbers
         
-        long long res = f(n,0,true,dp)%mod;
-        res = (res%mod * res%mod)%mod;
+        if(N==1) return 2*2; 
+	    if(N==2) return 3*3;
         
-        res%= mod;
+	    long long  first=2;
+	    long long  second=3;
+	    long long  third;
         
-        return res;
-    }
-    
-    int f(int n, int index, bool ctake,vector<vector<long long>>& dp)
-    {
-        if(index == n)
-        {
-           return 1;
-        }
-        
-        if(dp[index][ctake]!=-1)
-        {
-            return dp[index][ctake];
-        }
-        
-        long long sum = 0;
-        if(ctake)
-        {
-            sum = (sum + f(n,index+1,false,dp)%mod)%mod;
-        }
-        
-        sum = (sum +  f(n,index+1,true,dp)%mod)%mod;
-        
-        return dp[index][ctake] = sum;
+	    for(int i=2 ; i<N ; i++)
+	    {
+	        third = ((first % mod) + (second % mod)) % mod;
+	        first = second % mod;
+	        second = third % mod;
+	    }
+	    long long int ans= ((third % mod) * (third % mod))%mod;
+	    return ans;
     }
 };
