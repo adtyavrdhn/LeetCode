be5b7bfa7062e6df0ffbd7f87ff2577d871963df
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-06-29 01:04:46 +0530

    Time: 41 ms (79.67%), Space: 31.9 MB (87.54%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,40 +1,35 @@
-class Solution
-{
-    public:
-        int maxSatisfied(vector<int> &customers, vector<int> &grumpy, int minutes)
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) 
+    {
+        int n = grumpy.size();
+        int sum = 0;
+        for(int i=0;i<n;i++)
         {
-            int n = grumpy.size();
-            
-            int sum = 0;
-            
-            for (int i = 0; i < n; i++)
-                if (grumpy[i] == 0)
-                    sum += customers[i];
-
-            int i = 0,j = 0;
-            
-            int res = 0;
-            
-            while (j < n)
-            {
-                if (grumpy[j] == 1)
-                    sum += customers[j];
-                
-                if (j - i + 1 < minutes)
-                    j++;
-                
-                else if (j - i + 1 == minutes)
-                {
-                    res = max(res, sum);
-                    
-                    if (grumpy[i] == 1)
-                        sum -= customers[i];
-                    
-                    i++;
-                    j++;
-                }
-            }
-            
-            return res;
+            if(grumpy[i]==0)
+                sum+= customers[i];
         }
+        
+        int l=0,r=0,res=0;
+        
+        while(r<n)
+        {
+            if(grumpy[r]==1)
+                sum+= customers[r];
+            
+            if(r-l+1<minutes)
+                r++;
+            
+            else if(r-l+1 == minutes)
+            {
+                res = max(sum,res);
+                
+                if(grumpy[l]==1)
+                    sum-= customers[l];
+                l++,r++;
+            }
+        }
+        
+        return res;
+    }
 };
