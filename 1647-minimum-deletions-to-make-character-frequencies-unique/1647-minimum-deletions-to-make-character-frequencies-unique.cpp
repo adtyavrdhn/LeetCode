5a5f73dae7b4976bd5a98f170b02e1d49c7bb69b
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-06-28 23:20:26 +0530

    Time: 196 ms (12.15%), Space: 17.5 MB (21.80%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,28 +1,28 @@
-class Solution
-{
-    public:
-        int minDeletions(string s)
+class Solution {
+public:
+    int minDeletions(string s) 
+    {
+      unordered_map<char,int> mpp;
+        
+        for(auto c : s)
         {
-            vector<int> mpp(26, 0);
-            for (char c: s)
-            {
-                mpp[c - 'a']++;
-            }
-
-            int count = 0;
-            
-            unordered_set<int> st;
-            
-            for (int i = 0; i < 26; i++)
-            {
-                while (mpp[i] && st.find(mpp[i]) != st.end())
-                {
-                    mpp[i]--;
-                    count++;
-                }
-                st.insert(mpp[i]);
-            }
-
-            return count;
+            mpp[c]++;
         }
+        
+        unordered_set<int> st;
+        
+        int count = 0;
+        
+        for(auto i : mpp)
+        {
+            while(i.second > 0 && st.find(i.second)!=st.end())
+            {
+                i.second--;
+                count++;
+            }
+            st.insert(i.second);
+        }
+        
+        return count;
+    }
 };
