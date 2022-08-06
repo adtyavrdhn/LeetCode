147bd8d9092af4c43cf5995439a15c5da16f0adf
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-08-06 17:05:04 +0530

    Time: 28 ms (39.22%), Space: 17 MB (12.97%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    unordered_map<string,bool> mpp;
+    vector<int> dp;
+    bool wordBreak(string s, vector<string>& dict) 
+    {
+        int n = s.size();
+        
+        dp.resize(n,-1);
+        
+        for(auto i : dict)
+        {
+            mpp[i]=true;
+        }
+        
+        return f(s,0);
+    }
+    
+    bool f(string s, int index)
+    {
+        if(index==s.size())
+        {
+            return true;
+        }
+        
+        if(dp[index]!=-1)
+        {
+            return dp[index];
+        }
+        
+        for(int i=index;i<s.size();i++)
+        {
+            if(mpp[s.substr(index,i-index+1)] && f(s,i+1))
+            {
+                return dp[index] = true;
+            }
+        }
+        
+        return dp[index] = false;
+    }
+};
