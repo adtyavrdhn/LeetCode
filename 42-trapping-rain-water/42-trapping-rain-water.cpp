406a6c618833e5ed1ba6e5c3d3fbb1062c0c8f0b
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-08-09 17:14:26 +0530

    Time: 28 ms (40.79%), Space: 20.5 MB (25.01%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+    public:
+        int trap(vector<int> &arr)
+        {
+            int n = arr.size();
+
+            vector<int> pref(n, 0);
+            pref[0] = arr[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                pref[i] = max(arr[i], pref[i - 1]);
+            }
+
+            vector<int> suff(n, 0);
+
+            suff[n - 1] = arr[n - 1];
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                suff[i] = max(suff[i + 1], arr[i]);
+            }
+            
+            int water = 0;
+            for(int i=0;i<n;i++)
+            {
+                water+= min(pref[i],suff[i])-arr[i];
+            }
+
+//             for (auto i: pref)
+//                 cout << i << " ";
+
+//             cout << endl;
+
+//             for (auto i: suff)
+//                 cout << i << " ";
+
+//             cout << endl;
+
+            return water;
+        }
+};
