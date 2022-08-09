9a949d6f4ad6325e875d75308970b0bd13373db3
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-08-09 15:09:29 +0530

    Time: 106 ms (59.69%), Space: 9.3 MB (67.38%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,45 +1,45 @@
-class Solution {
-public:
-    int numFactoredBinaryTrees(vector<int>& arr) 
-    {
-        int n = arr.size();
-        map<int,long long int> mpp;
-        
-       long long int mod = 1e9 + 7;
-        
-        sort(arr.begin(),arr.end());
-        
-        for(auto i : arr)
+class Solution
+{
+    public:
+        int numFactoredBinaryTrees(vector<int> &arr)
         {
-            mpp[i]++;
-        }
-        
-        for(int i=1;i<n;i++)
-        {
-            long long int count = 0;
-            for(int j=0;j<i;j++)
+            map<int, long long int> mpp;
+            long long int mod = 1e9 + 7;
+            
+            sort(arr.begin(),arr.end());
+
+            for (auto i: arr)
             {
-                if(arr[i]%arr[j]==0)
-                {
-                    auto it = mpp.find(arr[i]/arr[j]);
-                    
-                    if(it!=mpp.end())
-                    {
-                        count += (it->second)*(mpp[arr[j]]);
-                    }
-                }
+                mpp[i]++;
             }
             
-            mpp[arr[i]]+= count;
+            int n = arr.size();
+
+            for (int i = 1; i< n; i++)
+            {
+                long long count = 0;
+                for (int j = 0; j < i; j++) 
+                {
+                    if(arr[i]%arr[j]==0)
+                    {
+                         auto it = mpp.find(arr[i]/arr[j]);
+                        
+                        if(it!=mpp.end())
+                        {
+                            count+= (it->second)*(mpp[arr[j]]);
+                        }
+                    }
+                }
+                
+                mpp[arr[i]]+=count;
+            }
+            
+            long long int res=0;
+            for(auto i : mpp)
+            {
+                res+= i.second;
+            }
+            
+            return res%mod;
         }
-        
-        long long int res = 0;
-        
-        for(auto i : mpp)
-        {
-            res+= i.second;
-        }
-        
-        return res%mod;
-    }
 };
