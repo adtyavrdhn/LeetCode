80287b265a5bddab83c89f144fed7486b5bac6d7
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-08-12 00:57:54 +0530

    Time: 17 ms (70.46%), Space: 10.1 MB (40.31%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,41 +1,53 @@
-class Solution
-{
-    public:
-        void solve(vector<vector < char>> &grid)
-        {
-            if (grid.size() == 0)
-                return;
-            int row = grid.size(), col = grid[0].size();
-
-           	//first row and last row
-            for (int i = 0; i < row; i++)
-            {
-                dfs(grid, i, 0);
-                dfs(grid, i, col - 1);
-            }
-           	//first col and last col
-            for (int j = 1; j < col - 1; j++)
-            {
-                dfs(grid, 0, j);
-                dfs(grid, row - 1, j);
-            }
-
-            for (int i = 0; i < row; ++i)
-                for (int j = 0; j < col; ++j)
-                    if (grid[i][j] == 'O') grid[i][j] = 'X';
-                    else if (grid[i][j] == '1') grid[i][j] = 'O';
-        }
-    void dfs(vector<vector < char>> &grid, int i, int j)
+class Solution {
+public:
+    vector<vector<int>> dir = {{-1,0},{0,-1},{1,0},{0,1}};
+    void f(vector<vector<char>>&board, int i, int j)
     {
-        if (grid[i][j] == 'O')
+        board[i][j]='#';
+        
+        for(int k=0;k<4;k++)
         {
-
-            grid[i][j] = '1';
-
-            if (i + 1 < grid.size()) dfs(grid, i + 1, j);
-            if (i > 1) dfs(grid, i - 1, j);
-            if (j + 1 < grid[i].size()) dfs(grid, i, j + 1);
-            if (j > 1) dfs(grid, i, j - 1);
+            int nx = i + dir[k][0];
+            int ny = j + dir[k][1];
+            
+            if(nx>=0 && ny>=0 && nx<board.size() && ny<board[nx].size() && board[nx][ny]=='O')
+            {
+                f(board,nx,ny);
+            }
+        }
+        
+    }
+    void solve(vector<vector<char>>& board) 
+    {
+        int m = board.size();
+        int n = board[0].size();
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 || j==0 || i == m-1 || j==n-1)
+                {
+                    if(board[i][j]=='O')
+                       f(board,i,j);
+                }
+            }
+        }
+        
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j]=='O')
+                {
+                    board[i][j]='X';
+                }
+                else if(board[i][j]=='#')
+                {
+                    board[i][j]='O';
+                }
+            }
         }
     }
 };
