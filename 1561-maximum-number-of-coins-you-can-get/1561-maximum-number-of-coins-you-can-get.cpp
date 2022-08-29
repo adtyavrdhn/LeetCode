28ea50d08bac191c216be077f0c5ca9dc3a953bd
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-08-29 23:31:56 +0530

    Time: 133 ms (95.38%), Space: 53.5 MB (8.43%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -3,30 +3,20 @@ public:
     int maxCoins(vector<int>& piles) 
     {
         int n = piles.size();
-        
         sort(piles.begin(),piles.end());
         
-        for(auto i : piles)
-        {
-            cout<<i<<" ";
-        }
-        
         int me = n-2;
-        int alice = n-1;
         int bob = 0;
-        
         
         int res = 0;
         
         while(bob < me)
         {
             res+= piles[me];
-            alice -= 2;
+            
             me-= 2;
             bob++;
         }
-        
-        // cout<<endl;
         
         return res;
     }
