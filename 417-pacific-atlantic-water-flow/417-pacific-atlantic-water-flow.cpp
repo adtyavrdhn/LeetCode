ff465012693999433a832062784ccd1d6850d4f6
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-08-31 12:18:38 +0530

    Time: 59 ms (72.04%), Space: 17.5 MB (77.12%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,60 +1,50 @@
-class Solution
-{
-    public:
-        vector<vector < int>> pacificAtlantic(vector<vector < int>> &heights)
-        {
-            vector<vector < int>> res;
-
-            if (!heights.size())
-                return res;
-
-            int row = heights.size();
-            int col = heights[0].size();
-
-            vector<vector < bool>> atlantic(row, vector<bool> (col, false));
-            vector<vector < bool>> pacific(row, vector<bool> (col, false));
-
-            for (int i = 0; i < row; i++)
-            {
-                dfs(heights, i, 0, INT_MIN, pacific);
-                dfs(heights, i, col - 1, INT_MIN, atlantic);
-            }
-
-            for (int j = 0; j < col; j++)
-            {
-                dfs(heights, 0, j, INT_MIN, pacific);
-                dfs(heights, row - 1, j, INT_MIN, atlantic);
-            }
-
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    if (pacific[i][j] && atlantic[i][j])
-                        res.push_back({ i,
-                            j });
-                }
-            }
-
-            return res;
-        }
-
-    void dfs(vector<vector < int>> &matrix, int x, int y, int prev, vector< vector< bool>> &ocean)
+class Solution {
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) 
     {
-        if (x < 0 || x >= matrix.size() || y < 0 || y >= matrix[0].size())
+        int m = heights.size();
+        int n = heights[0].size();
+        
+        vector<vector<bool>> pac(m, vector<bool> (n, false));
+        vector<vector<bool>> atl(m, vector<bool> (n,false));
+        
+        for(int i=0;i<m;i++)
+        {
+            f(pac,i,0,INT_MIN,heights);
+            f(atl,i,n-1,INT_MIN,heights);
+        }
+        for(int i=0;i<n;i++)
+        {
+            f(pac,0,i,INT_MIN,heights);
+            f(atl,m-1,i,INT_MIN,heights);
+        }
+        
+        int count = 0;
+        
+        vector<vector<int>> res;
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(pac[i][j] && atl[i][j])
+                    res.push_back({i,j});
+            }
+        }
+        
+        return res;
+        
+    }
+    void f(vector<vector<bool>>&ocean, int i, int j, int prev,vector<vector<int>>& heights)
+    {
+        if(i<0 || j<0 || i>= ocean.size() || j>=ocean[i].size() || ocean[i][j] || heights[i][j]<prev)
             return;
-
-        if (ocean[x][y])
-            return;
-
-        if (matrix[x][y] < prev)
-            return;
-
-        ocean[x][y] = true;
-
-        dfs(matrix, x + 1, y, matrix[x][y], ocean);
-        dfs(matrix, x - 1, y, matrix[x][y], ocean);
-        dfs(matrix, x, y + 1, matrix[x][y], ocean);
-        dfs(matrix, x, y - 1, matrix[x][y], ocean);
+        
+        ocean[i][j]=true;
+        
+        f(ocean,i+1,j,heights[i][j],heights);
+        f(ocean, i-1,j,heights[i][j],heights);
+        f(ocean,i,j+1,heights[i][j],heights);
+        f(ocean,i,j-1,heights[i][j],heights);
     }
 };
