9d1b4806ec573fbfd63054d754efae273f226fe3
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-09-02 18:14:29 +0530

    Time: 3 ms (59.29%), Space: 5.7 MB (96.66%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -5,8 +5,8 @@ public:
         int count = 0;
         while(n>0)
         {
-            count+= n&1;
-            n>>=1;
+            count++;
+            n = n & (n-1);
         }
         
         return count;
