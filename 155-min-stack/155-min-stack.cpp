5de26c849444fbfb3983e2346d0a616646bd29fb
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-09-07 21:07:19 +0530

    Time: 34 ms (56.69%), Space: 16.3 MB (71.80%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,32 +1,33 @@
 class MinStack {
 public:
-    multiset<int> st;
-    stack<int> arr;
-    MinStack() {
+    
+    vector<int> arr;
+    vector<int> min;
+    MinStack() 
+    {
         
     }
     
     void push(int val) 
     {
-        arr.push(val);
-        st.insert(val);
-        
+        arr.push_back(val);
+        if(min.empty() || val<min.back()) 
+            min.push_back(val);
+        else min.push_back(min.back());
     }
     
     void pop() 
     {
-        st.erase(st.find(arr.top()));
-        arr.pop();
+        arr.pop_back();
+        min.pop_back();
     }
     
-    int top() 
-    {
-        return arr.top();
+    int top() {
+        return arr.back();
     }
     
-    int getMin() 
-    {
-        return *st.begin();
+    int getMin() {
+        return min.back();
     }
 };
 
