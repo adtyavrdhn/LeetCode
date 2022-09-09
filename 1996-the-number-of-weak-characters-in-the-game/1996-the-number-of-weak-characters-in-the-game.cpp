175cdac7b0ec8043882d3c2d4999c02a3ec0c613
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-09-09 20:05:26 +0530

    Time: 1902 ms (5.05%), Space: 125.2 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -13,19 +13,18 @@ public:
     {
         sort(arr.begin(),arr.end(),comp);
         
-        int res =0;
+        int res = 0;
         int max_def = INT_MIN;
+        int max_att = INT_MIN;
         
         for(int i=0;i<arr.size();i++)
         {
-            if(arr[i][1] < max_def)
+            if(arr[i][1] < max_def && arr[i][0]<max_att)
             {
                 res++;
             }
-            else
-            {
-                max_def = arr[i][1];
-            }
+            max_def = max(max_def,arr[i][1]);
+            max_att = max(max_att,arr[i][0]);
         }
         
         return res;
