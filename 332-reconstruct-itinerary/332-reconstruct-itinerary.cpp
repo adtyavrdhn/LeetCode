89ab77387d6740c3462bfef3869e2a78d1206b13
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-09-15 17:30:08 +0530

    Time: 65 ms (16.12%), Space: 15.2 MB (34.99%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) 
+    {
+        map<string,multiset<string>> mpp;
+        
+        int n = tickets.size()+1;
+        
+        for(auto i : tickets)
+        {
+            string x = i[0];
+            string y = i[1];
+            
+            mpp[x].insert(y);
+        }
+        
+        vector<string> res;
+        
+        f("JFK",mpp,res);
+        
+        reverse(res.begin(),res.end());
+        
+        return res;
+    }
+    void f(string airport,map<string,multiset<string>>&mpp,vector<string>&res)
+    {
+        while (!mpp[airport].empty()) 
+        {
+            string next = *mpp[airport].begin();
+            
+            mpp[airport].erase(mpp[airport].begin());
+            
+            f(next, mpp,res);
+        }
+        
+        res.push_back(airport);
+    }
+};
