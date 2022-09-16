8b2beebd2e50c70cacb18e5f99531d384b347631
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-09-16 22:49:34 +0530

    Time: 806 ms (33.92%), Space: 120.1 MB (66.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<int>> dp;
     int maximumScore(vector<int>& nums, vector<int>& mult) 
     {
-       int n = nums.size();
-    int m = mult.size();
-       dp.resize(m+1, vector<int> (m+1,-1001));
-       return f(nums,mult,0,0,n-1);
+        int n = nums.size();
+        int m = mult.size();
+        dp.resize(m+1, vector<int> (m+1,-1001));
+        return f(nums,mult,0,0,n-1);
     }
     int f(vector<int>&nums, vector<int>&mult, int index, int i, int j)
     {
