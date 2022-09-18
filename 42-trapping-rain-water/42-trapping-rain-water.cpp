cca9e05658f611d0246e4709e8e1ce759b188ec0
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-09-18 19:12:20 +0530

    Time: 23 ms (65.92%), Space: 20.6 MB (25.17%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,33 +1,31 @@
-class Solution
-{
-    public:
-        int trap(vector<int> &arr)
+class Solution {
+public:
+    int trap(vector<int>& arr) 
+    {
+        int n = arr.size();
+        vector<int> pref(n,0);
+        vector<int> suff(n,0);
+        
+        pref[0]=arr[0], suff[n-1]=arr[n-1];
+        
+        for(int i=1;i<n;i++)
         {
-            int n = arr.size();
-
-            vector<int> pref(n, 0);
-            pref[0] = arr[0];
-
-            for (int i = 1; i < n; i++)
-            {
-                pref[i] = max(arr[i], pref[i - 1]);
-            }
-
-            vector<int> suff(n, 0);
-
-            suff[n - 1] = arr[n - 1];
-
-            for (int i = n - 2; i >= 0; i--)
-            {
-                suff[i] = max(suff[i + 1], arr[i]);
-            }
-            
-            int water = 0;
-            for(int i=0;i<n;i++)
-            {
-                water+= min(pref[i],suff[i])-arr[i];
-            }
-
-            return water;
+            pref[i] = max(pref[i-1],arr[i]);
         }
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            suff[i] = max(suff[i+1],arr[i]);
+        }
+        
+        int res = 0;
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            res+= min(pref[i],suff[i])-arr[i];
+        }
+        
+        return res;
+    }
 };
