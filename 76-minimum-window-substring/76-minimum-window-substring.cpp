17db8ee287b376204a0ecc6df0f59406697ec032
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-09-20 09:30:17 +0530

    Time: 51 ms (30.66%), Space: 16.3 MB (20.45%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -5,9 +5,8 @@ class Solution
         {
             if (s.length() < t.length())
             {
-                return ""; // impossible task
+                return "";	// impossible task
             }
-                
 
             int n = s.size();
             unordered_map<char, int> mpp;
@@ -16,7 +15,7 @@ class Solution
 
             for (int i = 0; i < t.size(); i++)
             {
-                mpp[t[i]]++; // adding the characters frequency to our map
+                mpp[t[i]]++;	// adding the characters frequency to our map
             }
 
             int count = mpp.size();
@@ -26,35 +25,35 @@ class Solution
 
             while (j < n)
             {
-                mpp[s[j]]--; // if we find a character that was a part of t and hence in our map, taking it into our substring will mean one less character to find in the substring
+                mpp[s[j]]--;	// if we find a character that was a part of t and hence in our map, taking it into our substring will mean one less character to find in the substring
                 if (mpp[s[j]] == 0)
-                    count--; // we found all instances of a character needed and that means one less character to find
+                    count--;	// we found all instances of a character needed and that means one less character to find
 
-                if (count == 0) // we found all our characters
+               	// we found all our characters
+
+                while (count == 0)	// now we shrink our window
                 {
-                    while (count == 0) // now we shrink our window
+                    if (j - i + 1 < res)	// if the current window is smaller
                     {
-                        if (j - i + 1 < res) // if the current window is smaller
-                        {
-                            res = j - i + 1;
-                            ans = s.substr(i, res);
-                        }
-                        
-                        if(mpp.find(s[i])!=mpp.end())
-                        mpp[s[i]]++; // shrinking the window means we are removing characters from our substring and so if that character was a part of the string t, that means we need to find that character elsewhere to make up for it
-
-                        if (mpp[s[i]] == 1)
-                        {
-                            count++; // we ended up removing a character from our answer substring, which would mean we need to make up for it and find it ahead
-                        }
-
-                        i++;
+                        res = j - i + 1;
+                        ans = s.substr(i, res);
                     }
+
+                    if (mpp.find(s[i]) != mpp.end())
+                        mpp[s[i]]++;	// shrinking the window means we are removing characters from our substring and so if that character was a part of the string t, that means we need to find that character elsewhere to make up for it
+
+                    if (mpp[s[i]] == 1)
+                    {
+                        count++;	// we ended up removing a character from our answer substring, which would mean we need to make up for it and find it ahead
+                    }
+
+                    i++;
                 }
+
                 j++;
             }
-            
-            if(res == INT_MAX)
+
+            if (res == INT_MAX)
             {
                 return "";
             }
