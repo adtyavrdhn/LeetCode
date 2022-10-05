2b3f07e1770ce9411a2526a905e90ae69ad1eae9
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-10-05 18:38:43 +0530

    Time: 43 ms (23.62%), Space: 14.6 MB (26.14%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,55 +1,35 @@
-class Solution
-{
-    public:
-    
-    unordered_map<string, bool> mpp;
-    vector<bool> dp;
-    
-    bool wordBreak(string s, vector<string> &dict)
+class Solution {
+public:
+    unordered_set<string> st;
+    int dp[301];
+    bool wordBreak(string s, vector<string>& dict) 
     {
-        int n = s.size();
-        dp.resize(n + 1, false);
-
-        for (auto i: dict)
+        memset(dp,-1,sizeof(dp));
+        for(auto i : dict)
+            st.insert(i);
+        
+        return f(s,0);
+    }
+    bool f(string s, int index)
+    {
+        if(index>=s.size())
         {
-            mpp[i] = true;
+            return true;
         }
-
-        dp[n] = true;
-
-        for (int i = n - 1; i >= 0; i--)
+        
+        if(dp[index]!=-1)
+            return dp[index];
+        
+        for(int i=index;i<s.size();i++)
         {
-            for (int j = i; j < n; j++)
+            string k = s.substr(index,i-index+1);
+            
+            if(st.find(k)!=st.end() && f(s,i+1))
             {
-                if (mpp[s.substr(i, j - i + 1)] && dp[j + 1])
-                {
-                    dp[i] = true;
-                }
+                return dp[index] = true;
             }
         }
-
-       return dp[0];
+        
+        return dp[index] = false;
     }
-   	//     bool f(string s, int index)
-   	//     {
-   	//         if(index==s.size())
-   	//         {
-   	//             return true;
-   	//         }
-
-   	//         if(dp[index]!=-1)
-   	//         {
-   	//             return dp[index];
-   	//         }
-
-   	//         for(int i=index;i < s.size();i++)
-   	//         {
-   	//             if(mpp[s.substr(index,i-index+1)] && f(s,i+1))
-   	//             {
-   	//                 return dp[index] = true;
-   	//             }
-   	//         }
-
-   	//         return dp[index] = false;
-   	//     }
 };
