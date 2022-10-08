66e09d822615407842fd803ad75bb3a1a4e52f1c
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-10-08 18:48:25 +0530

    Time: 1149 ms (8.62%), Space: 456.1 MB (5.04%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    int countSubstrings(string s) 
+    int countSubstrings(string s)
     {
-        int n = s.size();
-        
-        vector<vector<int>> dp(n,vector<int> (n,0));
-        
-        int res = s.size();
-        
-        for(int i=0;i<n;i++)
-            dp[i][i]=1;
-        
-        int max_len = 1;
-        
-        for(int i=n-1;i>=0;i--)
+        int res = 0;
+        for(int i=0;i<s.size();i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=i;j<s.size();j++)
             {
-                if(s[i]==s[j])
-                {
-                    if(j-i==1 || dp[i+1][j-1])
-                    {
-                        dp[i][j]=1;
-                         res++; 
-                    }
-                }
+                string temp = s.substr(i,j-i+1);
+                
+                if(ispall(temp))
+                    res++;
             }
         }
         
         return res;
+    }
+    bool ispall(string& s)
+    {
+        int i=0, j=s.size()-1;
+        
+        while(i<j)
+        {
+            if(s[i++]!=s[j--])
+                return false;
+        }
+        
+        return true;
     }
 };
