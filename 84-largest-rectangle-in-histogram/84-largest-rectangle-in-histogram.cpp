7e2f75110da4c2ded894bd16057f970c00536ae1
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-10-09 21:03:02 +0530

    Time: 408 ms (30.75%), Space: 75.3 MB (90.72%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -2,41 +2,32 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& arr) 
     {
-        int n = arr.size();
-        vector<int> left(n,0);
-        vector<int> right(n,0);
-        
         stack<int> st;
         
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() && arr[st.top()]>=arr[i])
-                st.pop();
-            
-            left[i] = st.empty() ? 0 : st.top()+1;
-            
-            st.push(i);
-        }
-        
-        while(!st.empty())
-            st.pop();
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st.empty() && arr[st.top()]>=arr[i])
-                st.pop();
-            
-            right[i] = st.empty() ? n-1 : st.top()-1;
-            
-            st.push(i);
-        }
+        int n = arr.size();
         
         int res = 0;
         
-        for(int i=0;i<n;i++)
+        for(int i=0;i<=n;i++)
         {
-            res = max(res, (right[i]-left[i]+1)*arr[i]);
+            while(!st.empty() && (i==n || arr[st.top()]>=arr[i]))
+            {
+                int height = arr[st.top()]; // the last height which will be stored in variable height will keep the tower we are currently calculating for
+                st.pop();
+                
+                int width = 0;
+                
+                if(st.empty())
+                    width = i;
+                
+                else width = i-st.top()-1; // st.top() is left smaller index while i is the right smaller index
+                
+                res = max(res, width*height);
+                
+            }
+            st.push(i);
         }
+        
         return res;
     }
 };
