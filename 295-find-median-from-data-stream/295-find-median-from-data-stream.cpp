8cd7d14c4418532d0667af162b3528a147e2edfa
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-10-09 22:42:20 +0530

    Time: 322 ms (98.82%), Space: 117.1 MB (18.53%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,37 +1,53 @@
 class MedianFinder {
+public:
     priority_queue<int> small;
     priority_queue<int,vector<int>,greater<int>> large;
-public:
     MedianFinder() {
         
     }
     
-    void addNum(int num) 
-    {
-        if(small.empty() || num<small.top()) small.push(num);
-        else large.push(num);
+    void addNum(int num) {
+        if(small.empty() || num<small.top())
+            small.push(num);
+        else
+            large.push(num);
         
-        if(small.size()>large.size() && small.size()-large.size()>1){
-            int top = small.top();
+        if(small.size() > large.size() && small.size() - large.size() > 1)
+        {
+            int n = small.top();
             small.pop();
-            large.push(top);
+            large.push(n);
         }
         
-        if(large.size()>small.size() && large.size()-small.size()>1){
-            int top = large.top();
+        if(large.size() > small.size() && large.size()-small.size() > 1)
+        {
+            int n = large.top();
             large.pop();
-            small.push(top);
+            small.push(n);
         }
     }
     
     double findMedian() 
     {
         double median;
-        if(small.size()==large.size()){
-            median = ((double)small.top()+(double)large.top())/2;
-        }else{
-            median = small.size()>large.size() ? small.top() : large.top();
+        
+        if(small.size() > large.size())
+            median = small.top();
+        
+        else if(large.size() > small.size())
+             median = large.top();
+        
+        else {
+           median = (large.top() + small.top()) / 2.0;
         }
+        
         return median;
     }
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
