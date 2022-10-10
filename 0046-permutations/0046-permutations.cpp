6cd801be6b8d0b24ca06a4072bfc0eed6f62d41f
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-10-10 21:23:59 +0530

    Time: 3 ms (84.72%), Space: 7.8 MB (47.16%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,33 +1,33 @@
-class Solution
-{
-    public:
-        vector<vector < int>> res;
-    vector<vector < int>> permute(vector<int> &nums)
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> permute(vector<int>& nums) 
     {
         int n = nums.size();
-        vector<bool> vis(n, false);
         vector<int> temp;
+        vector<bool> vis(n,false);
+        
         f(nums,temp,vis);
         
         return res;
     }
-    void f(vector<int> &nums, vector<int> &temp, vector< bool > &vis)
+    void f(vector<int>&nums, vector<int>&temp, vector<bool>&vis)
     {
-        if (temp.size() == nums.size())
+        if(temp.size()==nums.size())
         {
             res.push_back(temp);
             return;
         }
-
-        for (int i = 0; i < nums.size(); i++)
+        
+        for(int i=0;i<nums.size();i++)
         {
-            if (!vis[i]) 
+            if(!vis[i])
             {
                 vis[i]=true;
                 temp.push_back(nums[i]);
                 f(nums,temp,vis);
-                vis[i]=false;
                 temp.pop_back();
+                vis[i]=false;
             }
         }
     }
