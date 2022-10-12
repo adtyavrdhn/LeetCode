92bc1ea7ce5009bf4d41732eb5c603f174354af0
Aditya Vardhan <76904033+adtyavrdhn@users.noreply.github.com>
2022-10-13 01:52:39 +0530

    Time: 103 ms (7.40%), Space: 21.9 MB (23.05%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -5,11 +5,14 @@ public:
         sort(nums.begin(),nums.end());
         
         int res = 0;
-        for(int i=2;i<nums.size();i++)
+        int n = nums.size();
+        
+        for(int i=n-3;i>=0;i--)
         {
-            if(nums[i-1]+nums[i-2] > nums[i])
+            if(nums[i]+nums[i+1] > nums[i+2])
             {
-                res = max(res,nums[i-1]+nums[i-2]+nums[i]);
+                res = max(res,nums[i+1]+nums[i+2]+nums[i]);
+                return res;
             }
         }
         
