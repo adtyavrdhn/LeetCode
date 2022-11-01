80ff9dfacdd9c87acc3929c2155b0135f7ed77e3
adtyavrdhn <76904033+adtyavrdhn@users.noreply.github.com>
2022-11-01 14:12:06 +0530

    Added solution - LeetHub

---
+++ Quick Sort - GFG/quick-sort.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        if(low>=high)
+            return;
+            
+        int pivot = partition(arr,low,high);
+        quickSort(arr,low,pivot-1);
+        quickSort(arr,pivot+1,high);
+        
+    }
+    
+    public:
+    int partition (int arr[], int low, int high)
+    {
+        int i=low+1,j=high;
+        int pivot = arr[low];
+        
+        while(i<=j)
+        {
+            while(arr[i]<=pivot)
+            {
+                i++;
+            }
+            
+            while(arr[j]>pivot)
+            {
+                j--;
+            }
+            
+            if(i<j)
+                swap(arr[i],arr[j]);
+        }
+        
+        swap(arr[low],arr[j]);
+        
+        return j;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      Solution ob;
+    ob.quickSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
+// } Driver Code Ends
