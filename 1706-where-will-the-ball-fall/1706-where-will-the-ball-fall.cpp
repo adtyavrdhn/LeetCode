b07915eae684fcb5aff0b296d9518452706f7dc0
adtyavrdhn <76904033+adtyavrdhn@users.noreply.github.com>
2022-11-01 15:07:51 +0530

    Time: 66 ms (21.09%), Space: 21.4 MB (5.40%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,30 +1,34 @@
 class Solution
 {
     public:
+        map<int,map<int,int>> mpp;
         vector<int> findBall(vector<vector < int>> &grid)
         {
             int m = grid.size(), n = grid[0].size();
 
             vector<int> res(n, 0);
-
+            
+            
             for (int i = 0; i < n; i++)
             {
-                res[i] = f(grid, 0, i);	
+                res[i] = f(grid, 0, i);
             }
 
             return res;
         }
     int f(vector<vector < int>> &grid, int i, int j)
     {
-        if(i==grid.size())
+        if (i == grid.size())
             return j;
-        
+
         int nj = j + grid[i][j];
         
-        if(nj<0 || nj>=grid[0].size() || grid[i][j]!= grid[i][nj])
-            return -1;
+        if(mpp[i][j]!=0)
+            return mpp[i][j];
         
-        
-        return f(grid,i+1,nj);
+        if (nj < 0 || nj >= grid[0].size() || grid[i][j] != grid[i][nj])
+            return mpp[i][j] = -1;
+
+        return mpp[i][j] = f(grid, i + 1, nj);
     }
 };
