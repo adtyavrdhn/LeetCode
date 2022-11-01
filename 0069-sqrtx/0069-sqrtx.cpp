7077f84374dce8ade2d708314947ad2383779fce
adtyavrdhn <76904033+adtyavrdhn@users.noreply.github.com>
2022-11-01 20:51:38 +0530

    Time: 6 ms (44.94%), Space: 6 MB (24.76%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,11 +2,23 @@ class Solution {
 public:
     int mySqrt(int x) 
     {
-        long long int y = 0;
         
-        while(y*y<=x)
-            y++;
+        int low = 0, high = INT_MAX - 1;
+        long long int res = 0;
         
-        return y-1;
+        while(low<=high)
+        {
+            long long int mid = (low + (high-low)/2);
+            
+            if(mid*mid<=x)
+            {
+                res = max(res,mid);
+                low = mid + 1;
+            }
+            else
+                high = mid - 1;
+        }
+        
+        return res;
     }
 };
